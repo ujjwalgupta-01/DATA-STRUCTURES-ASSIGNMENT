95b04489c51486ea925a259d5f75eb46d49adf93
ujjwalgupta-01 <ugupta_be24@thapar.edu>
2025-09-02 01:27:32 +0530

    Update QUES_2.cpp

--- ASSIGNMENT_2/QUES_2.cpp
+++ ASSIGNMENT_2/QUES_2.cpp
@@ -4,21 +4,25 @@ using namespace std;
 int main(){
     int arr[]={64,34,25,12,22,11,90};
     int n=sizeof(arr)/4;
-
     
-for(int j=0;j<n;j++){
+    
+    for(int i=0;i<n-1;i++){
+    for(int j=0;j<n-i-1;j++){
+    bool swap=false;
 
-    for(int i=0;i<n;i++){
-        if(arr[i]>arr[i+1]){
-            int temp=arr[i];
-            arr[i]=arr[i+1];
-            arr[i+1]=temp;
+        if(arr[j]>arr[j+1]){
+            int temp=arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+            swap=true;
             
+        }
+        if(!swap){
+            break;
         }
     }
         
 }
-
     for(int i=0;i<n;i++){
         
         cout<<arr[i]<<" ";
